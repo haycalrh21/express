0db0b2568b16bfb07037bd4e2f3b7b141b0e16ae
Your Name <hrayhansyah@gmail.com>
2024-09-20 01:47:39 +0700

    ini apa ya

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,78 +4,81 @@ import asyncHandler from "../middlewares/asyncHandler.js";
 import { serialize } from "cookie";
 
 const signToken = (id) => {
-	return jwt.sign({ id }, process.env.JWT_SECRET, {
-		expiresIn: "6d",
-	});
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "6d",
+  });
 };
 
 const createSendToken = (user, statusCode, res) => {
-	const token = signToken(user._id);
-	// Set cookie with token
-	res.cookie("jwt", token, {
-		httpOnly: true,
-		secure: process.env.NODE_ENV === "production",
-		sameSite: "Strict",
-		maxAge: 6 * 24 * 60 * 60 * 1000, // 6 days
-	});
-
-	res.status(statusCode).json({
-		data: {
-			name: user.name,
-			email: user.email,
-			role: user.role,
-		},
-	});
+  const token = signToken(user._id);
+  // Set cookie with token
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // Ini harus sesuai dengan lingkungan Anda
+    sameSite: "Strict",
+    maxAge: 6 * 24 * 60 * 60 * 1000, // 6 days
+  });
+  res.status(statusCode).json({
+    data: {
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    },
+  });
 };
 
 export const register = asyncHandler(async (req, res) => {
-	const Admin = (await User.countDocuments()) === 0;
-	const role = Admin ? "admin" : "user";
+  const Admin = (await User.countDocuments()) === 0;
+  const role = Admin ? "admin" : "user";
 
-	const createUser = await User.create({
-		name: req.body.name,
-		email: req.body.email,
-		password: req.body.password,
-		role,
-	});
-	createSendToken(createUser, 201, res);
+  const createUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    role,
+  });
+  createSendToken(createUser, 201, res);
 });
-
 export const login = asyncHandler(async (req, res) => {
-	if (!req.body.email || !req.body.password) {
-		res.status(400).json({ message: "Invalid email or password" });
-		return;
-	}
+  try {
+    if (!req.body.email || !req.body.password) {
+      res.status(400).json({ message: "Invalid email or password" });
+      return;
+    }
 
-	const user = await User.findOne({ email: req.body.email });
+    const user = await User.findOne({ email: req.body.email });
 
-	if (user && (await user.comparePassword(req.body.password))) {
-		createSendToken(user, 200, res);
-	} else {
-		res.status(401).json({ message: "Invalid email or password" });
-	}
+    if (user && (await user.comparePassword(req.body.password))) {
+      createSendToken(user, 200, res);
+    } else {
+      res.status(401).json({ message: "Invalid email or password" });
+    }
+  } catch (error) {
+    console.error("Login Error:", error); // Tambahkan log untuk menangkap kesalahan
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 export const getUser = asyncHandler(async (req, res) => {
-	const user = await User.findById(req.user._id).select("-password");
-	if (user) {
-		res.status(200).json({
-			data: user,
-		});
-	} else {
-		res.status(404).json({ message: "User not found" });
-	}
+  const user = await User.findById(req.user._id).select("-password");
+  if (user) {
+    res.status(200).json({
+      data: user,
+    });
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
 });
 
 export const logout = asyncHandler(async (req, res) => {
-	const cookie = serialize("jwt", "", {
-		httpOnly: true,
-		secure: process.env.NODE_ENV === "production",
-		sameSite: "strict",
-		maxAge: -1,
-		path: "/",
-	});
+  const cookie = serialize("jwt", "", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: -1,
+    path: "/",
+  });
 
-	res.setHeader("Set-Cookie", cookie);
-	res.status(200).json({ message: "Logged out successfully" });
+  res.setHeader("Set-Cookie", cookie);
+  res.status(200).json({ message: "Logged out successfully" });
 });
